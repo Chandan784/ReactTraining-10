5fe4ff39c927593ba285489a1c39f4a721863b00
chandan <chandansamantaray784@gmail.com>
2025-03-31 20:40:10 +0530

    v1

---
+++ index.js
@@ -0,0 +1,196 @@
+// var num = 2;
+
+// {
+//   let x = 2;
+
+//   console.log(x);
+//   console.log(num);
+// }
+
+// if (1) {
+//   var x = 4;
+//   let y = 3;
+// }
+
+// /console.log(y);
+
+// function hii() {
+//   var num2 = 5;
+// }
+
+// console.log(num2);
+
+// const x = 4;
+
+// x = 5;
+// let x = true;
+
+// console.log(typeof x);
+
+// let age = 12;
+
+// if (age > 18) {
+//   console.log("you can vote");
+// } else {
+//   console.log("you can not vote");
+// }
+
+// let fullMark = 600;
+// let mark = 200;
+// if (mark > 300) {
+//   console.log("A");
+// } else if (mark > 200) {
+//   console.log("B");
+// } else if (mark > 150) {
+//   console.log("C");
+// } else {
+//   console.log("fail");
+// }
+
+// if (1) {
+//   if (0) {
+//     if (1) {
+//     } else {
+//     }
+//   } else {
+//     console.log("hiii");
+//   }
+// } else {
+// }
+
+// for (let x = 1; x < 11; x++) {
+//   console.log(x);
+// }
+
+// let x = 1;
+
+// while (x < 11) {
+//   console.log(x);
+//   x++;
+// }
+
+// let x = 1;
+
+// do {
+//   console.log(x);
+//   x++;
+// } while (x > 11);
+
+// let dayNum = 4;
+
+// switch (dayNum) {
+//   case 0:
+//     console.log("sunday");
+
+//     break;
+//   case 1:
+//     console.log("Monday");
+
+//     break;
+//   case 2:
+//     console.log("tue");
+
+//     break;
+//   case 3:
+//     console.log("wed");
+
+//     break;
+//   case 4:
+//     console.log("thur");
+
+//     break;
+//   case 5:
+//     console.log("fri");
+
+//     break;
+//   case 6:
+//     console.log("sat");
+
+//     break;
+
+//   default:
+//     console.log("invalid");
+// }
+//function creation
+// function addNum() {
+//   let x = 4;
+//   let y = 5;
+//   console.log(x + y);
+// }
+
+// addNum();
+// addNum();
+// addNum();
+//parameter
+// function addNum(x, y) {
+//   console.log(x + y);
+// }
+//function argument
+// addNum(4, 8);
+// addNum(10, 10);
+// addNum(20, 1000);
+
+//function with return
+// function greeting(name) {
+
+//   return `hello ${name}`;
+//   //string template
+// }
+
+// let result = greeting("akash");
+
+// console.log(result);
+
+// function akash() {
+//   var x = 2;
+
+//   {
+//     console.log(x);
+//   }
+// }
+
+// akash();
+
+// console.log(x);
+
+//arrow function
+
+// function addNum(a, b) {
+//   console.log(a + b);
+// }
+
+// let addNum = (a, b) => {
+//   console.log(a + b);
+// };
+
+// let addNum = (a => console.log(a);
+
+// addNum(10);
+
+// function getSqr(num) {
+//   return num * num;
+// }
+
+// console.log(getSqr(10));
+
+// console.log(ans);
+
+// let getSqr = (num )=> num*num
+
+// let value = getSqr(10);
+// console.log(value);
+
+// function functionName (){
+
+// }
+
+//  let fun1  =   function x(){
+//   console.log("x");
+
+// }
+
+// function fun2(fun){
+// fun()
+// }
+
+// fun2(fun1)
