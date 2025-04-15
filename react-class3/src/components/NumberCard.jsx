a213c900af5b77d2e5688c258e7a09b9df954fbd
chandan <chandansamantaray784@gmail.com>
2025-04-15 22:10:21 +0530

    state management

--- react-class3/src/components/NumberCard.jsx
+++ react-class3/src/components/NumberCard.jsx
@@ -1,3 +1,5 @@
+import style from "./NumberCard.module.css";
+
 function NumberCard({ x }) {
   return <h1>{x}</h1>;
 }
---
+++ react-class4/src/App.jsx
@@ -0,0 +1,27 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  let [number, setNumber] = useState(0);
+  function increment() {
+    setNumber(number + 1);
+  }
+
+  function decrement() {
+    setNumber(number - 1);
+  }
+
+  return (
+    <>
+      <div className="" style={{ fontSize: "100px" }}>
+        {number}
+      </div>
+      <button onClick={increment}>add</button>
+      <button onClick={decrement}>sub</button>
+    </>
+  );
+}
+
+export default App;
