a068fc1d818eb50458f1fcbacd253f725e1a9153
chandan <chandansamantaray784@gmail.com>
2025-05-15 21:53:25 +0530

    internal working- hoisting

--- dom/index.js
+++ dom/index.js
@@ -10,3 +10,68 @@
 
 // myTitle.style.backgroundColor = "red";
 // console.log(myTitle);
+
+//query selector, class, id,tag
+// document.getElementsByTagName()
+
+//class add and remove
+
+// let boxContainer = document.querySelector("#box");
+
+// boxContainer.classList.remove("box2");
+// boxContainer.classList.add("box");
+
+// console.log(boxContainer);
+
+// let changeBgBtn = document.querySelector("#changeBgBtn");
+
+// changeBgBtn.addEventListener("click", () => {
+//   boxContainer.classList.toggle("x");
+// });
+
+// console.log(boxContainer);
+
+// let body = document.querySelector("body");
+
+// let allH1 = document.querySelectorAll("h1");
+
+// window.addEventListener("scroll", () => {
+//   allH1.forEach((h1) => {
+//     h1.style.fontSize = "100px";
+//   });
+// });
+
+// hoisting
+
+// 01010101
+
+// global
+
+// console.log(x);
+// console.log(y);
+
+// console.log(add);
+
+// function add() {
+//   console.log("jdhdjd");
+// }
+
+// var x = 2;
+// let y = 3;
+
+// console.log(x);
+
+// console.log(add);
+//compile - code execution
+//executable - 10101010101010
+
+//error check
+//
+
+//code execution - global execution context - memory phase, execution pahse
+
+// memeory phase  x:2 , y:unin, add: add() {
+//   console.log("jdhdjd");
+// }
+
+//hoisting
