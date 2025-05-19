66fb90db950e397cc7bded0570c441740e71af93
chandan <chandansamantaray784@gmail.com>
2025-05-19 22:15:08 +0530

    array sum

--- arraypractice.js
+++ arraypractice.js
@@ -9,9 +9,17 @@
 
 // console.log(lat, lon);
 
-function useDeatails() {
-  return ["chandan", 26, "computer science"];
-}
-let [name, age, subject] = useDeatails();
+// function useDeatails() {
+//   return ["chandan", 26, "computer science"];
+// }
+// let [name, age, subject] = useDeatails();
 
-console.log(name, age, subject);
+// console.log(name, age, subject);
+
+let scores = [200, 14, 50, 60];
+let total = 0;
+for (let i = 0; i < scores.length; i++) {
+  total = total + scores[i];
+}
+
+console.log(total);
